7d1e85508f26a717368e001c418c908ef8d85169
krzysztofchojka <krzysio.chojka@gmail.com>
2025-05-26 00:48:53 +0200

    testing functions

--- test2.py
+++ test2.py
@@ -3,8 +3,8 @@ import numpy as np
 from scipy.io import wavfile
 
 # 1) Wczytaj
-fs, data = wavfile.read("./sdr-tests/SDRconnect_IQ_20250520_164031_137110000HZ.wav")
-#fs, data = wavfile.read("./SDRconnect_IQ_20250520_113333_99900000HZ.wav")
+#fs, data = wavfile.read("./sdr-tests/SDRconnect_IQ_20250520_164031_137110000HZ.wav")
+fs, data = wavfile.read("./sdr-tests/SDRconnect_IQ_20250520_113333_99900000HZ.wav")
 
 print(f"Liczba próbek: {data.shape[0]}, liczba kanałów: {data.shape[1]}")
 print(f"Częstotliwość próbkowania: {fs} Hz")
@@ -12,7 +12,7 @@ print(f"Częstotliwość próbkowania: {fs} Hz")
 # 2) Normalizuj (jeśli int16)
 if data.dtype == np.int16:
     data = data.astype(np.float32) / 32768.0
-print(data)
+#print(data)
 
 # 3) Wyodrębnij I/Q
 I = data[:,0]
@@ -22,11 +22,11 @@ iq = I + 1j*Q
 # 4) Wybierz sygnał y
 y = I # lub iq
 
-# 5) Oblicz liczbę próbek dla 5 ms
-duration_sec = 0.05
+# 5) Oblicz liczbę próbek dla 0.4 ms
+duration_sec = 0.004
 samples = int(duration_sec * fs)
 
-# 6) Wytnij pierwsze 5 ms
+# 6) Wytnij pierwsze x ms
 y_short = y[:samples]
 t_short = np.arange(samples) / fs
 
@@ -35,17 +35,15 @@ N = len(y_short)
 
 
 # === Generate a 1D signal ===
-sig = sa.generate_signal(sa.SignalType.Square, freq=5, sampling_rate=500, duration=1)
-#sig=y_short
+#sig = sa.generate_signal(sa.SignalType.Square, freq=5, sampling_rate=500, duration=1)
+sig=y_short
 
-temp=sa.DFT(sig, fs, True)
-print(sig)
-print('=================')
-print(temp)
+temp=sa.DFT(sig, fs, False)
+sa.showComplexSignal(temp, 500, "title", 'x', "Y")
+
 
 new=[]
 for el in temp:
-    print(el)
     new.append(float(el.imag))
 sig=new
 #exit()
@@ -53,14 +51,22 @@ sig=new
 kernel1d = [1/3, 1/3, 1/3]
 filt = sa.filter1D(sig, kernel1d)
 
+sa.showComplexSignal(filt, 500, "title", 'x', "Y")
+
+image0 = [sig for _ in range(13)]
+
 # === Prepare a 2D image from the filtered 1D signal ===
 # Replicate the filtered 1D signal vertically to make a 2D image
-image_2d = [filt for _ in range(100)]  # 100 rows of the 1D filtered signal
+image_2d = [filt for _ in range(10)]  # 100 rows of the 1D filtered signal
 
 # === Apply 2D filtering ===
 kernel2d = [[1/9]*3 for _ in range(3)]  # 3x3 averaging kernel
 filtered_image = sa.filter2D(image_2d, kernel2d)
+sa.showComplexSignal(filtered_image, 500, "title", 'x', "Y")
 
 # === Show images ===
+print(image_2d)
+
+sa.showImage(image0, title="Image from DFT")
 sa.showImage(image_2d, title="Image from 1D Filtered Signal")
 sa.showImage(filtered_image, title="2D Filtered Image")
